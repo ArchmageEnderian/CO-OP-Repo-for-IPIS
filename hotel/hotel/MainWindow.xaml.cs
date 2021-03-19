8f20c2f3cd13ada1c98d84a8559fb4baf64356d8
TwilightFoxy <62305710+TwilightFoxy@users.noreply.github.com>
2021-03-19 23:09:42 +0300

    Добавлено отображение времени(фича)

--- hotel/hotel/MainWindow.xaml.cs
+++ hotel/hotel/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace hotel
         public MainWindow()
         {
             InitializeComponent();
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
         }
 
         private void new_guest_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -33,28 +34,27 @@ namespace hotel
         {
             MessageBox.Show("Це кнопка minus_guest");
         }
-
         private void maybe_guest_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             MessageBox.Show("Це кнопка maybe_guest");
         }
-
         private void all_guest_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             MessageBox.Show("Це кнопка all_guest");
         }
-
         private void MouseEnter(object sender, MouseEventArgs e)
         {
             TextBlock btn = (TextBlock)sender;
             btn.Background = Brushes.AntiqueWhite;
-
         }
-
         private void MouseLeave(object sender, MouseEventArgs e)
         {
             TextBlock btn = (TextBlock)sender;
             btn.Background = Brushes.White;
+        }
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            time.Text = DateTime.Now.ToString("HH:mm:ss");
         }
     }
 }
