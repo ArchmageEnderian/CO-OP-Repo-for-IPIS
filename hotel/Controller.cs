feda6196ade858214c7dd4bfbc2d6c00398c4fec
『 Enderian 』 <iggor.malakhov@gmail.com>
2021-03-19 22:43:49 +0300

    Controller v0.0.0

---
+++ hotel/Controller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Controller
+{
+	private Controller(string userName)
+	{
+
+	}
+
+	public List<UserModel> Users { get; }
+
+	public UserModel CurrentUser { get; }
+
+	public void Save()
+    {
+
+    }
+
+	public void SetNewUserData(string genderName, DateTime birthDate, int passortSeries, int passortNumber, string phoneNumber, int roomNumber, bool withChildren, int amountOfResidents, DateTime arrivalDate, DateTime departureDate)
+    {
+
+    }
+
+	private List<UserModel> GetUsersData() // ???
+    {
+
+    }
+
+
+
+}
