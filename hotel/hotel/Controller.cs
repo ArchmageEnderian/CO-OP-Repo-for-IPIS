92c58120bb803465100426fc1895499f3c21b6c5
『 Enderian 』 <iggor.malakhov@gmail.com>
2021-05-29 01:05:50 +0300

    Изменена структура

--- hotel/hotel/Controller.cs
+++ hotel/hotel/Controller.cs
@@ -6,7 +6,11 @@ namespace MainController
 {
     public class Controller
     {
-        DB database = new DB();
+        private DB database;
+        public Controller()
+        {
+            database = new DB();
+        }
 
         public bool AddPersons(string surname, string name, string sex, string birthday, int age, string Serial, string Serial_nums, string telephone, string room, string num, string input, string output)
         {
@@ -16,9 +20,7 @@ namespace MainController
 
         public List<string> ShowUsers()
         {
-            List<string> allString = database.select("client JOIN human ON human.id = client.human_id JOIN check_settling ON check_settling.id = client.check_id JOIN interval_arrive ON interval_arrive.id = check_settling.interval_id JOIN room ON room.id = check_settling.room_id JOIN gender ON gender.id = human.gender_id",
-                "human.surname, human.name, gender.title, human.birthdate,human.pasport_s, human.pasport_n,human.phone, interval_arrive.arrival_date, interval_arrive.departure_date, room.number, room.capacity");
-            return allString;
+            return database.GetUsers();
         }
 /*
 SELECT human.surname, human.name, gender.title, human.birthdate,human.pasport_s, human.pasport_n,human.phone, interval_arrive.arrival_date, interval_arrive.departure_date, room.number, room.capacity
